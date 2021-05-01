3df2e29eb07a8c7adb5bc579efb6a8c44539e90c
Jaewan Park <hueypark@line.games>
2021-05-01 15:31:20 +0900

    MapExporter(Editor Toolbar Button Plugin) 추가

---
+++ Plugins/MapExporter/Source/MapExporter/Private/MapExporterCommands.cpp
@@ -0,0 +1,12 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MapExporterCommands.h"
+
+#define LOCTEXT_NAMESPACE "FMapExporterModule"
+
+void FMapExporterCommands::RegisterCommands()
+{
+	UI_COMMAND(PluginAction, "MapExporter", "Execute MapExporter action", EUserInterfaceActionType::Button, FInputGesture());
+}
+
+#undef LOCTEXT_NAMESPACE
