bec3b008a46d8e2189d875e32aa99caef7fc6bcf
Jaewan Park <hueypark@line.games>
2021-05-01 15:01:37 +0900

    샘플 프로젝트 생성

---
+++ Source/PluginExample/PluginExample.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PluginExample.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PluginExample, "PluginExample" );
+
+DEFINE_LOG_CATEGORY(LogPluginExample)
+ 
---
+++ Source/PluginExample/PluginExampleGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PluginExampleGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APluginExampleGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APluginExampleGameMode();
+};
+
+
+
