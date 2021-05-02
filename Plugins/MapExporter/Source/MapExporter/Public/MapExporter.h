8a00fe02f77c27f292bbcd74e2b183f723d95d3f
Jaewan Park <hueypark@line.games>
2021-05-02 09:44:22 +0900

    플러그인 생성

---
+++ Plugins/MapExporter/Source/MapExporter/Public/MapExporter.h
@@ -0,0 +1,14 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+class FMapExporterModule : public IModuleInterface
+{
+public:
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
