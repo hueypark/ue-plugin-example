471248648aca0d2d127517612ac05f04547f55c0
Jaewan Park <hueypark@line.games>
2021-05-15 15:56:49 +0900

    ItemInfo 데이터 테이블 추가

---
+++ Source/PluginExample/ItemInfo.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "Engine/DataTable.h"
+
+#include "ItemInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FItemInfo : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 ID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName DisplayName;
+};
